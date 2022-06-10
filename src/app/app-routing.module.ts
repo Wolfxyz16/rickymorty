ada4103a23660bb469f71667ba3e48264c5546f1
Yeray Li Loaiza <yerayliloaiza@gmail.com>
2022-06-10 21:53:29 +0200

    characters route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterViewComponent as Character} from './character-view/character-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'characters', component: Character}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
